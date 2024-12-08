ab5f8d3715ea165c926416c192efb8ec521cc144
ZokszY <136007010+ZokszY@users.noreply.github.com>
2024-12-08 03:22:45 +0100

    Small changes

--- docs/bash/export_notes.sh
+++ docs/bash/export_notes.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 pandoc --from=markdown+rebase_relative_paths -s -o notes.pdf --metadata-file=metadata.yaml --toc --pdf-engine=pdflatex $(cat notes.txt)
-# pandoc --from=markdown+smart -s -o notes.pdf --metadata-file=metadata.yaml --toc --pdf-engine=pdflatex --filter=rebase_paths.py $(cat notes.txt)
