e357cddb8e6ef74fb81710ae153810a3dd739f77
ZokszY <136007010+ZokszY@users.noreply.github.com>
2024-12-08 04:39:25 +0100

    Update workflow

--- docs/bash/export_notes.sh
+++ docs/bash/export_notes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc --from=markdown+rebase_relative_paths -s -o notes.pdf --metadata-file=metadata.yaml --toc --pdf-engine=pdflatex $(cat notes.txt)
+pandoc --from=markdown+rebase_relative_paths -s -o pdf/notes.pdf --metadata-file=metadata.yaml --toc --pdf-engine=pdflatex $(cat notes.txt)
